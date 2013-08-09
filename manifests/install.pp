f8d682b6c4f06bd93c788eaac2262a46ec82e456
Joshua Keroes <joshua.keroes@integratelecom.com>
2013-08-09 16:22:27 -0700

    unwrap build-essentials. They don't exist on centos

--- manifests/install.pp
+++ manifests/install.pp
@@ -15,16 +15,24 @@
 # Sample Usage:
 #
 class perlbrew::install {
+  if ! defined(Package['autoconf'])  { package { 'autoconf':  ensure => installed } }
+  if ! defined(Package['automake'])  { package { 'automake':  ensure => installed } }
+  if ! defined(Package['binutils'])  { package { 'binutils':  ensure => installed } }
+  if ! defined(Package['bison'])     { package { 'bison':     ensure => installed } }
+  if ! defined(Package['flex'])      { package { 'flex':      ensure => installed } }
+  if ! defined(Package['gcc'])       { package { 'gcc':       ensure => installed } }
+  if ! defined(Package['gcc-c++'])   { package { 'gcc-c++':   ensure => installed } }
+  if ! defined(Package['gettext'])   { package { 'gettext':   ensure => installed } }
+  if ! defined(Package['libtool'])   { package { 'libtool':   ensure => installed } }
+  if ! defined(Package['make'])      { package { 'make':      ensure => installed } }
+  if ! defined(Package['patch'])     { package { 'patch':     ensure => installed } }
+  if ! defined(Package['pkgconfig']) { package { 'pkgconfig': ensure => installed } }
 
-  if !defined (Package['build-essential'])
-  { package
-    { 'build-essential':ensure => present, }
-  }
-
-  if !defined (Package['wget'])
-  { package
-    { 'wget':ensure => present, }
-  }
+  # Other packages required to build a proper Perl 
+  if ! defined(Package['readline-devel']) { package { 'readline-devel': ensure => installed } }
+  if ! defined(Package['openssl-devel'])  { package { 'openssl-devel':  ensure => installed } }
+  if ! defined(Package['zlib-devel'])     { package { 'zlib-devel':     ensure => installed } }
+  if ! defined(Package['wget'])           { package { 'wget':           ensure => installed } }
 
   file {
     $perlbrew::params::perlbrew_bin:
@@ -32,6 +40,23 @@ class perlbrew::install {
       group   => root,
       mode    => '0755',
       source  => "puppet:///modules/${module_name}/perlbrew",
-      require => [ Package['build-essential'], Package['wget'] ],
-  }
+      require => [
+        Package['wget'],
+        Package['autoconf'],
+        Package['automake'],
+        Package['binutils'],
+        Package['bison'],
+        Package['flex'],
+        Package['gcc'],
+        Package['gcc-c++'],
+        Package['gettext'],
+        Package['libtool'],
+        Package['make'],
+        Package['patch'],
+        Package['pkgconfig'],
+        Package['readline-devel'],
+        Package['openssl-devel'],
+        Package['zlib-devel'],
+      ],
+    }
 }
